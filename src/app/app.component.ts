83d42fc70008e2c5fd269f0930f749fd00707b81
akshkin <akshaya.kini12@gmail.com>
2025-01-29 14:46:44 +0100

    Create base layout

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,11 +1,14 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { NavComponent } from './core/nav/nav.component';
+import { AuthModalComponent } from './user/auth-modal/auth-modal.component';
+import { ModalComponent } from './shared/modal/modal.component';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, NavComponent, AuthModalComponent, ModalComponent],
   templateUrl: './app.component.html',
-  styleUrl: './app.component.css'
+  styleUrl: './app.component.css',
 })
 export class AppComponent {
   title = 'clips-angular';
