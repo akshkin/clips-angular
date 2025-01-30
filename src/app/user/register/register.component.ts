e8825f6ddb84852f34065c06ed862fdd4c0f1647
akshkin <akshaya.kini12@gmail.com>
2025-01-30 10:36:41 +0100

    Create login and register components

---
+++ src/app/user/register/register.component.ts
@@ -0,0 +1,22 @@
+import { Component, inject } from '@angular/core';
+import { ReactiveFormsModule, FormBuilder, Validators } from '@angular/forms';
+import { CommonModule } from '@angular/common';
+
+@Component({
+  selector: 'app-register',
+  imports: [ReactiveFormsModule, CommonModule],
+  templateUrl: './register.component.html',
+  styleUrl: './register.component.css',
+})
+export class RegisterComponent {
+  formBuilder = inject(FormBuilder);
+
+  form = this.formBuilder.nonNullable.group({
+    name: ['', [Validators.required]],
+    age: [18],
+    email: [''],
+    password: [''],
+    confirmPassword: [''],
+    phoneNumber: [''],
+  });
+}
