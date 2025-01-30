bd982d0d285fd80dc5976ac391f2f8e6ba19630e
akshkin <akshaya.kini12@gmail.com>
2025-01-30 10:58:13 +0100

    Create a reusable input component

---
+++ src/app/shared/input/input.component.ts
@@ -0,0 +1,14 @@
+import { Component, input } from '@angular/core';
+import { FormControl, ReactiveFormsModule } from '@angular/forms';
+
+@Component({
+  selector: 'app-input',
+  imports: [ReactiveFormsModule],
+  templateUrl: './input.component.html',
+  styleUrl: './input.component.css',
+})
+export class InputComponent {
+  control = input.required<FormControl>();
+  type = input('text');
+  placeholder = input('');
+}
