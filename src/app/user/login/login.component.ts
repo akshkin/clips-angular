a39e6684bda097b5afb839342ecc7beb6da0af4f
akshkin <akshaya.kini12@gmail.com>
2025-01-30 20:22:54 +0100

    Add functionality to login form

--- src/app/user/login/login.component.ts
+++ src/app/user/login/login.component.ts
@@ -1,11 +1,37 @@
-import { Component } from '@angular/core';
+import { Component, inject, signal } from '@angular/core';
+import { ReactiveFormsModule, FormBuilder, Validators } from '@angular/forms';
+import { InputComponent } from '../../shared/input/input.component';
+import { AlertComponent } from '../../shared/alert/alert.component';
 
 @Component({
   selector: 'app-login',
-  imports: [],
+  imports: [ReactiveFormsModule, InputComponent, AlertComponent],
   templateUrl: './login.component.html',
-  styleUrl: './login.component.css'
+  styleUrl: './login.component.css',
 })
 export class LoginComponent {
+  formBuilder = inject(FormBuilder);
 
+  form = this.formBuilder.nonNullable.group({
+    email: ['', [Validators.required, Validators.email]],
+    password: [
+      '',
+      [
+        Validators.required,
+        Validators.pattern(
+          /^(?=.*\d)(?=.*[a-z])(?=.*[A-Z])(?=.*[a-zA-Z]).{8,}$/
+        ),
+      ],
+    ],
+  });
+
+  showAlert = signal(false);
+  alertMessage = signal('Please wait while your account is being created');
+  alertColor = signal('blue');
+
+  login() {
+    this.showAlert.set(true);
+    this.alertMessage.set('Loggin in....');
+    this.alertColor.set('blue');
+  }
 }
