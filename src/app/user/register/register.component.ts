5265fef814cc0d897b5cd84201cff5b52b338c97
akshkin <akshaya.kini12@gmail.com>
2025-01-30 14:03:53 +0100

    Add validators for inputs

--- src/app/user/register/register.component.ts
+++ src/app/user/register/register.component.ts
@@ -1,10 +1,11 @@
 import { Component, inject } from '@angular/core';
 import { ReactiveFormsModule, FormBuilder, Validators } from '@angular/forms';
 import { CommonModule } from '@angular/common';
+import { InputComponent } from '../../shared/input/input.component';
 
 @Component({
   selector: 'app-register',
-  imports: [ReactiveFormsModule, CommonModule],
+  imports: [ReactiveFormsModule, CommonModule, InputComponent],
   templateUrl: './register.component.html',
   styleUrl: './register.component.css',
 })
@@ -12,11 +13,19 @@ export class RegisterComponent {
   formBuilder = inject(FormBuilder);
 
   form = this.formBuilder.nonNullable.group({
-    name: ['', [Validators.required]],
-    age: [18],
-    email: [''],
-    password: [''],
-    confirmPassword: [''],
+    name: ['', [Validators.required, Validators.minLength(3)]],
+    age: [18, [Validators.required, Validators.min(18)]],
+    email: ['', [Validators.required, Validators.email]],
+    password: [
+      '',
+      [
+        Validators.required,
+        Validators.pattern(
+          /^(?=.*\d)(?=.*[a-z])(?=.*[A-Z])(?=.*[a-zA-Z]).{8,}$/
+        ),
+      ],
+    ],
+    confirmPassword: ['', [Validators.required]],
     phoneNumber: [''],
   });
 }
