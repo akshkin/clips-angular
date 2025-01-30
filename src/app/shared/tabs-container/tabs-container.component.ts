1f5b8363e68d721def657dde1d8c785c6229d6c8
akshkin <akshaya.kini12@gmail.com>
2025-01-30 09:45:33 +0100

    Add comments

--- src/app/shared/tabs-container/tabs-container.component.ts
+++ src/app/shared/tabs-container/tabs-container.component.ts
@@ -1,9 +1,10 @@
 import { AfterContentInit, Component, contentChildren } from '@angular/core';
 import { TabComponent } from '../tab/tab.component';
+import { NgClass } from '@angular/common';
 
 @Component({
   selector: 'app-tabs-container',
-  imports: [],
+  imports: [NgClass],
   templateUrl: './tabs-container.component.html',
   styleUrl: './tabs-container.component.css',
 })
@@ -11,6 +12,16 @@ export class TabsContainerComponent implements AfterContentInit {
   tabs = contentChildren(TabComponent);
 
   ngAfterContentInit() {
-    console.log(this.tabs);
+    const activeTab = this.tabs().find((tab) => tab.isActive() === true);
+    if (!activeTab) {
+      this.selectTab(this.tabs()[0]);
+    }
+  }
+
+  selectTab(tab: TabComponent) {
+    this.tabs().forEach((tab) => tab.isActive.set(false));
+    tab.isActive.set(true);
+    // to avoid changing the url when tab is selected as they are links -- an alternative to event.preventDefault
+    return false;
   }
 }
