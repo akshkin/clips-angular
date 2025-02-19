a4ea30ec50245526259ef2ef40da949ea73d0781
akshkin <akshaya.kini12@gmail.com>
2025-02-19 12:01:55 +0100

    Add guard to routes requiring authenticated users

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -5,6 +5,9 @@ import { ManageComponent } from './views/manage/manage.component';
 import { UploadComponent } from './views/upload/upload.component';
 import { ClipComponent } from './views/clip/clip.component';
 import { NotFoundComponent } from './views/not-found/not-found.component';
+import { AuthGuard, redirectUnauthorizedTo } from '@angular/fire/auth-guard';
+
+const redirectUnauthorizedToHome = () => redirectUnauthorizedTo('/');
 
 export const routes: Routes = [
   {
@@ -20,14 +23,18 @@ export const routes: Routes = [
     component: ManageComponent,
     data: {
       authOnly: true,
+      authGuardPipe: redirectUnauthorizedToHome,
     },
+    canActivate: [AuthGuard],
   },
   {
     path: 'upload',
     component: UploadComponent,
     data: {
       authOnly: true,
+      authGuardPipe: redirectUnauthorizedToHome,
     },
+    canActivate: [AuthGuard],
   },
   {
     path: 'clip/:id',
